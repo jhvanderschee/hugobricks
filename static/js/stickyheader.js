f7210159a596d17ffc0b250556a214901b45e233
Joost van der Schee <joost@vdschee.nl>
2023-06-20 14:59:25 +0200

    update

--- static/js/stickyheader.js
+++ static/js/stickyheader.js
@@ -18,7 +18,7 @@ window.addEventListener('scroll', function ( event ) {
 		//console.log( 'debounced' );
         var doc = document.documentElement;
         var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-        if(top>200) document.querySelector('header').classList.add('sticky');
+        if(top>50) document.querySelector('header').classList.add('sticky');
         else document.querySelector('header').classList.remove('sticky');
 
 	});
