242a03f7b861dab3adf458c2475b52d0717d30b4
Joost van der Schee <joost@vdschee.nl>
2023-06-23 01:34:14 +0200

    update

--- themes/hugobricks/static/js/stickyheader.js
+++ themes/hugobricks/static/js/stickyheader.js
@@ -14,7 +14,6 @@ window.addEventListener('scroll', function ( e ) {
 	timeout = window.requestAnimationFrame(function () {
 
         // Run our scroll functions
-		//console.log( 'debounced' );
         var doc = document.documentElement;
         var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
         if(top>50) document.querySelector('header').classList.add('sticky');
