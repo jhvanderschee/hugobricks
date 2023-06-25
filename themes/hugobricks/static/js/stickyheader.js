159331fc6186eca41b2bbe394a3a32eef3816a2b
Joost van der Schee <joost@vdschee.nl>
2023-06-25 17:02:53 +0200

    update

--- themes/hugobricks/static/js/stickyheader.js
+++ themes/hugobricks/static/js/stickyheader.js
@@ -1,6 +1,5 @@
 // Setup a timer
 var timeout;
-var lastScrollTop = 0;
 
 // Listen for resize events
 const ev = new Event('scroll');
@@ -17,16 +16,9 @@ window.addEventListener('scroll', function ( e ) {
         // Run our scroll functions
         var doc = document.documentElement;
         var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-        var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-        if (st > lastScrollTop) {
-           // downscroll code
-           if(top>0) document.querySelector('header').classList.add('sticky');
-        } else if (st < lastScrollTop) {
-           // upscroll code
-           if(top<1) document.querySelector('header').classList.remove('sticky');
-        } // else was horizontal scroll
-        lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-        
+        if(top>100) document.querySelector('header').classList.add('sticky');
+        else document.querySelector('header').classList.remove('sticky');
+
 	});
 
 }, false);
