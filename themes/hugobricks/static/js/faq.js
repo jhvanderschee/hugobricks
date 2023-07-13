fa43ff0e5f36e5ed601e6c0bf488c1df7a71067e
Joost van der Schee <joost@vdschee.nl>
2023-07-13 23:51:29 +0200

    update

--- themes/hugobricks/static/js/faq.js
+++ themes/hugobricks/static/js/faq.js
@@ -1,17 +1,17 @@
-const els = document.querySelectorAll("details");
+const els = document.querySelectorAll("details summary");
 els.forEach( function(el) {
   el.addEventListener("click", (e) => {
-    if (el.hasAttribute("open")) { // since it's not closed yet, it's open!
+    if (el.parentElement.hasAttribute("open")) { // since it's not closed yet, it's open!
       e.preventDefault(); // stop the default behavior, meaning - the hiding
-      el.classList.add("closing"); // add a class which applies the animation in CSS
+      el.parentElement.classList.add("closing"); // add a class which applies the animation in CSS
     }
   });
   // when the "close" animation is over
-  el.addEventListener("animationend", (e) => {
-    if (el.classList.contains("closing")){
+  el.parentElement.addEventListener("animationend", (e) => {
+    if (el.parentElement.classList.contains("closing")){
       if (e.animationName === "closedetails") {
-        el.removeAttribute("open"); // close the element
-        el.classList.remove("closing"); // remove the animation
+        el.parentElement.removeAttribute("open"); // close the element
+        el.parentElement.classList.remove("closing"); // remove the animation
       }
     }
   });
