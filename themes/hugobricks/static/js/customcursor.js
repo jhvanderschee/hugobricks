246e77de66e1ad302d532f020279853a63847acf
Joost van der Schee <joost@vdscheenl>
2024-09-25 14:19:58 +0200

    update

--- themes/hugobricks/static/js/customcursor.js
+++ themes/hugobricks/static/js/customcursor.js
@@ -11,19 +11,23 @@ var xp = startx, yp = starty;
 
 // set the momentum with setInterval function
 var loop = setInterval(function(){
-    // change 12 to alter damping higher is slower
-    xp += ((mouseX - xp)/6);
-    yp += ((mouseY - yp)/6);
-    document.body.style.setProperty('--x',xp+'px');
-    document.body.style.setProperty('--y',yp+'px');
+    if(!document.body.classList.contains('mobilemenu')) {
+        // change 12 to alter damping higher is slower
+        xp += ((mouseX - xp)/6);
+        yp += ((mouseY - yp)/6);
+        document.body.style.setProperty('--x',xp+'px');
+        document.body.style.setProperty('--y',yp+'px');
+    }
 }, 30);
 
 document.onmousemove = function(e) {
-    document.body.classList.remove('hidecursor');
-    mouseX = e.clientX;
-    mouseY = e.clientY+window.scrollY;
-    clearTimeout(timer);
-    timer=setTimeout(mouseStopped,3000);   
+    if(!document.body.classList.contains('mobilemenu')) {
+        document.body.classList.remove('hidecursor');
+        mouseX = e.clientX;
+        mouseY = e.clientY+window.scrollY;
+        clearTimeout(timer);
+        timer=setTimeout(mouseStopped,3000);   
+    }
 }
 
 function mouseStopped(){    
@@ -31,11 +35,13 @@ function mouseStopped(){
 }
 
 const onMouseOver = function (e) {
-    var cursor = getComputedStyle(e.target).cursor;
-    if(cursor=='pointer') {
-        document.body.classList.add('cursorpointer');
-    } else {
-        document.body.classList.remove('cursorpointer');
+    if(!document.body.classList.contains('mobilemenu')) {
+        var cursor = getComputedStyle(e.target).cursor;
+        if(cursor=='pointer') {
+            document.body.classList.add('cursorpointer');
+        } else {
+            document.body.classList.remove('cursorpointer');
+        }
     }
 };
 
